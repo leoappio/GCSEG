b3e8a1b8dab4b336878272fd60274c8ada93a719
leo.appio <leonardo.appio@gmail.com>
2024-03-13 09:41:20 -0300

    adicionados resultados na tab2

--- GCSEG/Form2.cs
+++ GCSEG/Form2.cs
@@ -1,4 +1,6 @@
-﻿namespace GCSEG
+﻿using System.Security.Cryptography;
+
+namespace GCSEG
 {
     public partial class Form2 : Form
     {
@@ -16,6 +18,13 @@
 
         private void RealizarCalculos()
         {
+            var Kmod = _parametros.Kmod1 * _parametros.Kmod2 * _parametros.Kmod3;
+            var gamac = 1.4;
+            var gamat = 1.8;
+            var gamav = 1.8;
+
+
+            // Tab 1
             label1.Text = _parametros.ClasseMadeira;
 
             label2.Text = $"fcok: {_parametros.Fcok / 10:F2} KN/cm²";
@@ -29,6 +38,18 @@
             label8.Text = $"Kmod2:  {_parametros.Kmod2:F2}";
 
             label9.Text = $"Kmod3:  {_parametros.Kmod3:F2}";
+
+
+            //Tab 2
+            var fcod = ((Kmod * _parametros.Fcok / 10) / gamac);
+            var ftod = ((Kmod * _parametros.Ftok / 10) / gamat);
+            var fvd = ((Kmod * _parametros.Fvk / 10) / gamav);
+            var Eef = ((_parametros.E / 10) * Kmod);
+
+            label10.Text = $"fcod: {fcod:F2} KN/cm²";
+            label11.Text = $"ftod: {ftod:F2} KN/cm²";
+            label12.Text = $"fvd: {fvd:F2} KN/cm²";
+            label13.Text = $"Eef: {Eef:F2} KN/cm²";
         }
 
         private void groupBox5_Enter(object sender, EventArgs e)
